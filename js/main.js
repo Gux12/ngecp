e25c91ca227894acee8939a9c087683b77c1152d
Gux12 <gx199492@qq.com>
2016-05-03 21:34:39 +0800

    第一个页面动画完成

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
-    // 获取窗口宽度
-    winWidth=window.innerWidth;
-    winHeight=window.innerHeight;
-    // if (window.innerWidth)
-    //     winWidth = window.innerWidth;
-    // else if ((document.body) && (document.body.clientWidth))
-    //     winWidth = document.body.clientWidth;
-    // // 获取窗口高度
-    // if (window.innerHeight)
-    //     winHeight = window.innerHeight;
-    // else if ((document.body) && (document.body.clientHeight))
-    //     winHeight = document.body.clientHeight;
-    // 通过深入 Document 内部对 body 进行检测，获取窗口大小
-    // if (document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth) {
-    //     winHeight = document.documentElement.clientHeight;
-    //     winWidth = document.documentElement.clientWidth;
-    // }
+    // 获取窗口宽度，高度
+    winWidth = window.innerWidth;
+    winHeight = window.innerHeight;
     $('body').css('height', winHeight);
     $('body').css('width', winWidth);
     $(window).resize(function() {
         $('body').css('height', window.innerHeight);
         $('body').css('width', window.innerWidth);
     })
+
+    // var isBordered = 0;
+    // window.onscroll = function(event) {
+    //     /* Act on the event */
+    //     if ($(this).scrollTop() && 　!isBordered) {
+    //         $('.navbar-mynavbar').css('box-shadow', '0 5px 5px -3px rgba(0, 0, 0, 0.12)');
+    //         isBordered = 1;
+    //     } else if (!$(this).scrollTop()) {
+    //         isBordered = 0;
+    //         $('.navbar-mynavbar').css('box-shadow', '0 0 0 0 rgba(0, 0, 0, 0.12)');
+    //     };
+    // }
+    // 
+    $('#hello1').animo( { animation: 'fadeInLeft' , duration: 5 } );
+    $('#hello2').animo( { animation: 'fadeInRight' , duration: 5 } );
+    $('#hello-input').animo( { animation: 'fadeInUp' , duration: 5 } );
 });
